aa50fe81b2d901de6da752d83ad14cb8241ec07d
Hamza Ikine <hamzaikine@gmail.com>
2021-02-22 13:59:40 -0500

    Initial Commit

---
+++ Shared/HydrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrationApp.swift
+//  Shared
+//
+//  Created by Hamza Ikine on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct HydrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
